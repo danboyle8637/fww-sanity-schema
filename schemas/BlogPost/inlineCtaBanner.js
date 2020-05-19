3a44aa9659438e9c9344f14e3ea272bdc4dd1139
Dan Boyle <danboyle8637@gmail.com>
2020-05-18 20:30:39 -0400

    Make a small change to get my banner data into my blog post data so I can make custom banners.

--- schemas/BlogPost/inlineCtaBanner.js
+++ schemas/BlogPost/inlineCtaBanner.js
@@ -5,22 +5,11 @@ export default {
   options: { collapsible: true },
   fields: [
     {
-      name: 'bannerId',
-      title: 'Banner Id:',
-      description: 'This helps the app know which banner to show in the post.',
-      type: 'array',
-      of: [{ type: 'string' }],
-      options: {
-        list: [
-          { title: 'FWW Monthly', value: 'fwwMonthly' },
-          { title: 'FWW Reset', value: 'fwwReset' },
-          { title: 'Open Workout', value: 'openWorkout' },
-          { title: 'Open Q and A', value: 'openQA' },
-          { title: 'Plan Smart Eat Real', value: 'planSmartEatReal' },
-          { title: 'Nutrition Coaching', value: 'privateNutritionCoaching' },
-          { title: 'FWW LIVE', value: 'fwwLive' },
-        ],
-      },
+      name: 'bannerData',
+      title: 'Banner Data:',
+      description: 'Which banner should we pull data from?',
+      type: 'reference',
+      to: [{ type: 'ctaBannerCards' }],
     },
   ],
 }
