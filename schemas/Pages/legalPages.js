1d6bd0363071b4ee9a537679f021975052ff2d29
Dan Boyle <danboyle8637@gmail.com>
2020-06-02 14:09:23 -0400

    Added seo fields to the pages that need it.

--- schemas/Pages/legalPages.js
+++ schemas/Pages/legalPages.js
@@ -14,6 +14,19 @@ export default {
       type: 'string',
     },
     {
+      name: 'seoTitle',
+      title: 'SEO Title:',
+      description: 'Write a title for the sales letter.',
+      type: 'string',
+    },
+    {
+      name: 'seoDescription',
+      title: 'SEO Description:',
+      description: 'Write a short description for SEO reasons.',
+      type: 'text',
+      rows: 4,
+    },
+    {
       name: 'slug',
       title: 'Page Path:',
       description: 'Autogenerate from title.',
