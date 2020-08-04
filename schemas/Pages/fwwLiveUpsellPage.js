1df0bd574c739ad4368198e30f6ea93063229b3c
Dev Dan <danboyle8637@gmail.com>
2020-08-04 15:34:25 -0400

    Cleaned up the fww live upsell page.

--- schemas/Pages/fwwLiveUpsellPage.js
+++ schemas/Pages/fwwLiveUpsellPage.js
@@ -23,39 +23,6 @@ export default {
       type: "number",
     },
     {
-      name: "fwwLiveIphoneImage",
-      title: "FWW LIVE iPhone Image:",
-      type: "inPageImage",
-    },
-    {
-      name: "fwwLiveBullets1",
-      title: "FWW LIVE Class Schedule Bullets:",
-      description:
-        "This is the first chunk of bullets that explain the huge class schedule",
-      type: "copyBlock",
-    },
-    {
-      name: "fwwLiveBullets2",
-      title: "FWW LIVE Custom Workout Schedule Bullets:",
-      description:
-        "This is the second chunk of bullets they build their own workout schedule.",
-      type: "copyBlock",
-    },
-    {
-      name: "fwwLiveBullets3",
-      title: "FWW LIVE Results Bullets:",
-      description:
-        "This is the third chunk of bullets that explain how they can get results.",
-      type: "copyBlock",
-    },
-    {
-      name: "fwwLiveBullets4",
-      title: "FWW LIVE Community Bullets:",
-      description:
-        "This is the first chunk of bullets that explain the amazing community we have.",
-      type: "copyBlock",
-    },
-    {
       name: "fwwWhyUpsell",
       title: "Why Offer Upsell:",
       description: "Be transparent about why we are making this offer.",
