c46fb2e10860400ac0f20185305e7d8d8f61e84a
Dev Dan <danboyle8637@gmail.com>
2021-04-03 16:51:32 -0400

    Fixed up the YouTube schema for blog posts.

--- schemas/BlogPost/youTube.js
+++ schemas/BlogPost/youTube.js
@@ -1,14 +1,20 @@
 export default {
-  name: 'youTube',
-  title: 'YouTube Embed:',
-  type: 'object',
+  name: "youTube",
+  title: "YouTube Embed:",
+  type: "object",
   fields: [
     {
-      name: 'videoId',
-      title: 'Video Id:',
+      name: "videoTitle",
+      title: "Video Title:",
+      description: "What is the title of this video?",
+      type: "string",
+    },
+    {
+      name: "videoId",
+      title: "Video Id:",
       description:
-        'Just the video Id: watch?v=oPVte6aMprI - The part after the v=',
-      type: 'string',
+        "Just the video Id: watch?v=oPVte6aMprI - The part after the v=",
+      type: "string",
     },
   ],
 }
